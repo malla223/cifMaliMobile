d82f64b42893a7428fb5452cc868a56a54e67caa
Mohamed Lamine Koné <mohamedlamine.kone@odc.local>
2021-12-20 16:28:38 +0000

    testtttt

--- Prototype/src/app/user/user.page.ts
+++ Prototype/src/app/user/user.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-user',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserPage implements OnInit {
 
-  constructor() { }
+  constructor(public toastCtrl: ToastController,
+  private alertC: AlertController,
+    private load: LoadingController) { }
 
   ngOnInit() {
   }
+  
+  async calcul(value) {
+    const load = await this.load.create({
+      message: "Calcul en cours..."
+    });
+    load.present();
+  }
 
+  async alertV(){
+    await this.alertC.create({
+      header:"Alert",
+      message: "Demande Envoyer",
+      buttons:[
+      {
+        text:"Retour"
+      }
+      ]
+    }).then(res => res.present());
+  }
 }
+
+
